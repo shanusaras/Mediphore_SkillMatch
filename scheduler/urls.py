f3b02f3358c3e9df15fc92061eb7fc202156cc7f
shanusaras <shanusaras123@gmail.com>
2025-10-06 00:11:23 +0530

    Fix resources assignment and status management

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('', ProjectListView.as_view(), name='projects'),
     path('tasks', TaskListView.as_view(), name='tasks'),
+    path('projects/<int:project_id>/tasks/', ProjectTaskListView.as_view(), name='project_tasks'),
     path('task-detail/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
     path('task/<int:pk>/assign/', AssignResourceView.as_view(), name='assign_resource'),
 ]
--- scheduler/views.py
+++ scheduler/views.py
@@ -15,6 +15,21 @@ class TaskListView(ListView):
     template_name = 'tasks.html'
     context_object_name = 'tasks'
 
+class ProjectTaskListView(ListView):
+    model = Task
+    template_name = 'tasks.html'
+    context_object_name = 'tasks'
+
+    def get_queryset(self):
+        project_id = self.kwargs.get('project_id')
+        return Task.objects.filter(project_id=project_id)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        project_id = self.kwargs.get('project_id')
+        context['project'] = get_object_or_404(Project, id=project_id)
+        return context
+
 class TaskDetailView(DetailView):
     model = Task
     template_name = 'taskDetails.html'
@@ -24,22 +39,34 @@ class TaskDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         task = self.object  # current task
 
-        # Filter resources that match at least one required skill AND are available
+        # Get IDs of resources already assigned to other tasks during this time
+        assigned_resource_ids = Task.objects.filter(
+            start_time__lt=task.end_time,
+            end_time__gt=task.start_time,
+            assigned_resource__isnull=False  # Only consider tasks with assigned resources
+        ).exclude(pk=task.pk).values_list('assigned_resource', flat=True)
+
+        # Filter available resources, excluding already assigned ones
         matching_resources = Resource.objects.filter(
             skills__in=task.required_skills.all(),
             availabilities__start_time__lte=task.start_time,
             availabilities__end_time__gte=task.end_time,
             availabilities__status='available'
-        ).distinct()
+        ).exclude(id__in=assigned_resource_ids).distinct()
 
-        # Add to context
         context['matching_resources'] = matching_resources
         return context
 
+        # Ensure resource has ALL required skills
+        # for skill in task.required_skills.all():
+        #     matching_resources = matching_resources.filter(skills=skill)
+
+       
 class AssignResourceView(View):
     def post(self, request, pk):
         task = get_object_or_404(Task, pk=pk)
         resource_id = request.POST.get("resource_list")
+        
         if resource_id:
             resource = get_object_or_404(Resource, id=resource_id)
             # Assign resource to task
